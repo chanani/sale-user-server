36747726d09990f709158348a31ab4267d232dc8
CHANHAN <130114269+chanani@users.noreply.github.com>
2025-04-22 21:44:48 +0900

    feat : created_by, updated_by 자동으로 등록될 수 있도록 설정파일 추가

---
+++ src/main/java/com/sale/hot/global/web/auditorAware/CustomAuditorAware.java
@@ -0,0 +1,33 @@
+package com.sale.hot.global.web.auditorAware;
+
+import com.sale.hot.entity.user.User;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.util.Optional;
+
+/**
+ * CreatedBy or LastModifiedBy 에 변경한 사용자 식별자 매핑
+ * */
+@Component
+@Slf4j
+public class CustomAuditorAware implements AuditorAware<Long> {
+    @Override
+    public Optional<Long> getCurrentAuditor() {
+        log.info("CustomAuditorAware.getCurrentAuditor() 실행");
+        ServletRequestAttributes attr = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        if (attr != null) {
+            HttpServletRequest request = attr.getRequest();
+            Object user = request.getAttribute("loginUser");
+            if (user instanceof User u) {
+                log.info("CustomAuditorAware USER : {}", u.getId());
+                return Optional.ofNullable(u.getId());
+            }
+        }
+        return Optional.empty();
+    }
+}
