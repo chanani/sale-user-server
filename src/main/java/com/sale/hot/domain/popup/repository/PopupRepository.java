b3d671bae84ea5d660c4550f7da2d5a14e041c10
CHANHAN <130114269+chanani@users.noreply.github.com>
2025-04-27 21:26:47 +0900

    feat :  Popup Entity 생성

---
+++ src/main/java/com/sale/hot/domain/popup/repository/PopupRepository.java
@@ -0,0 +1,7 @@
+package com.sale.hot.domain.popup.repository;
+
+import com.sale.hot.entity.popup.Popup;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PopupRepository extends JpaRepository<Popup, Long> {
+}
---
+++ src/main/java/com/sale/hot/entity/popup/Popup.java
@@ -0,0 +1,34 @@
+package com.sale.hot.entity.popup;
+
+import com.sale.hot.entity.BaseEntity;
+import com.sale.hot.entity.common.constant.BooleanYn;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@DynamicInsert
+@DynamicUpdate
+@Table(name = "popup")
+@Builder
+public class Popup extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "active")
+    private BooleanYn active;
+
+}
