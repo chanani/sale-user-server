dc484f4c299f521e9a0554e1fa162f0b3e3860a5
CHANHAN <130114269+chanani@users.noreply.github.com>
2025-04-29 19:22:35 +0900

    feat : Notification Controller, Service, Repository 생성

---
+++ src/main/java/com/sale/hot/controller/notification/NotificationApiController.java
@@ -0,0 +1,11 @@
+package com.sale.hot.controller.notification;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "Notification API Controller", description = "사용자 알람 관련 API를 제공합니다.")
+@RestController(value = "/api/v1")
+@RequiredArgsConstructor
+public class NotificationApiController {
+}
