49087842bf86a7a8aa7534c52812128edacd83a1
CHANHAN <130114269+chanani@users.noreply.github.com>
2025-04-29 19:15:11 +0900

    chore : CommonController 내부 변경

--- src/main/java/com/sale/hot/controller/common/CommonController.java
+++ src/main/java/com/sale/hot/controller/common/CommonController.java
@@ -1,41 +1,15 @@
 package com.sale.hot.controller.common;
 
-import com.sale.hot.global.annotation.NoneAuth;
-import com.sale.hot.global.exception.dto.ErrorCode;
-import com.sale.hot.global.exception.dto.ErrorCodeData;
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
 
-
-@Controller
-@Profile({"local", "dev"})
+@Tag(name = "Common API Controller", description = "공통으로 사용하는 API를 제공합니다.")
+@RestController(value = "/api/v1")
+@RequiredArgsConstructor
 public class CommonController {
 
-    @NoneAuth
-    @GetMapping("/error-codes")
-    public String getErrorCodes(Model model) {
-        List<ErrorCodeData> errorCodes = ErrorCode.getErrorCodes();
 
-        List<Object> httpErrorCodes = new ArrayList<>(
-                Arrays.asList(
-                        ErrorCodeData.error(400, "Bad Request", "클라이언트의 잘못된 요청으로 인한 에러"),
-                        ErrorCodeData.error(404, "Not Found", "찾을 수 없음"),
-                        ErrorCodeData.error(405, "Method Not Allowed", "리소스가 이 메서드를 지원하지 않음"),
-                        ErrorCodeData.error(500, "Internal Server Error", "예기치 못한 에러"),
-                        ErrorCodeData.error(502, "Bad Gateway", "게이트웨이 또는 프록시 역할을 하는 서버가 통신 체인의 다른 서버로부터 유효하지 않거나 잘못된 응답을 받음")
-                )
-        );
-
-
-        model.addAttribute("errorCodes", errorCodes);
-        model.addAttribute("httpCodes", httpErrorCodes);
-        return "error-codes";
-    }
 
 }
