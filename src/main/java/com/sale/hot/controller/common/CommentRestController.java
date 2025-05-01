9278e03f55892bee09de3fc4c7ffe8c92cbb31c9
CHANHAN <130114269+chanani@users.noreply.github.com>
2025-05-01 15:21:14 +0900

    feat : Security filter 추가

---
+++ src/main/java/com/sale/hot/controller/common/CommentRestController.java
@@ -0,0 +1,11 @@
+package com.sale.hot.controller.common;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "Common API Controller", description = "공통으로 사용하는 API를 제공합니다.")
+@RestController
+@RequiredArgsConstructor
+public class CommentRestController {
+}
