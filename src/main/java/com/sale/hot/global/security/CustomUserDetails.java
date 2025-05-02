83f5ca8c225bf087704a26efe82a4edceab008a5
CHANHAN <130114269+chanani@users.noreply.github.com>
2025-05-02 22:10:30 +0900

    feat : CustomUserDetail 클래스 생성

---
+++ src/main/java/com/sale/hot/global/security/CustomUserDetails.java
@@ -0,0 +1,23 @@
+package com.sale.hot.global.security;
+
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Collection;
+
+@Getter
+public class CustomUserDetails extends User {
+
+    private final Long id;
+
+    public CustomUserDetails(String username,
+                             String password,
+                             Collection<? extends GrantedAuthority> authorities,
+                             Long id) {
+        super(username, password, authorities);
+        this.id = id;
+    }
+
+
+}
