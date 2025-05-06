d7c0b5c0813191e6ce96ccee9e4cd8c7441c8016
CHANHAN <130114269+chanani@users.noreply.github.com>
2025-05-06 16:43:40 +0900

    feat : 등급 생성 API 작업

--- src/main/java/com/sale/hot/domain/grade/repository/GradeRepository.java
+++ src/main/java/com/sale/hot/domain/grade/repository/GradeRepository.java
@@ -3,6 +3,7 @@ package com.sale.hot.domain.grade.repository;
 import com.sale.hot.entity.common.constant.StatusType;
 import com.sale.hot.entity.grade.Grade;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,4 +11,8 @@ import java.util.Optional;
 public interface GradeRepository extends JpaRepository<Grade, Long> {
     Optional<Grade> findFirstByStatusOrderByRankingAsc(StatusType statusType);
     List<Grade> findByStatusOrderByRankingAsc(StatusType statusType);
+    boolean existsByNameAndStatus(String name, StatusType statusType);
+
+    @Query("SELECT MAX(g.ranking) FROM Grade g WHERE g.status = :statusType")
+    Optional<Integer> findMaxRanking(StatusType statusType);
 }
--- src/main/java/com/sale/hot/domain/grade/service/DefaultGradeService.java
+++ src/main/java/com/sale/hot/domain/grade/service/DefaultGradeService.java
@@ -1,9 +1,12 @@
 package com.sale.hot.domain.grade.service;
 
 import com.sale.hot.domain.grade.repository.GradeRepository;
+import com.sale.hot.domain.grade.service.dto.request.GradeCreateRequest;
 import com.sale.hot.domain.grade.service.dto.response.GradeResponse;
 import com.sale.hot.entity.common.constant.StatusType;
 import com.sale.hot.entity.grade.Grade;
+import com.sale.hot.global.exception.OperationErrorException;
+import com.sale.hot.global.exception.dto.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -24,5 +27,31 @@ public class DefaultGradeService implements GradeService{
         return gradeRepository.findByStatusOrderByRankingAsc(StatusType.ACTIVE).stream()
                 .map(GradeResponse::new)
                 .toList();
+    }
+
+    @Override
+    @Transactional
+    public void addGrade(GradeCreateRequest request) {
+        // 등급명 중복 체크
+        checkGradeName(request.name());
+
+        // 순서 조회(제일 마지막 순번으로 등록)
+        Integer ranking = gradeRepository.findMaxRanking(StatusType.ACTIVE).orElse(0);
+
+        // 등급 등록
+        Grade newEntity = request.toEntity(++ranking);
+        Grade saveGrade = gradeRepository.save(newEntity);
+    }
+
+    /**
+     * 등급명 중복 체크
+     * 중복일 경우 바로 예외 발생
+     *
+     * @param name 등급명
+     */
+    private void checkGradeName(String name) {
+        if (gradeRepository.existsByNameAndStatus(name, StatusType.ACTIVE)) {
+            throw new OperationErrorException(ErrorCode.EXISTS_GRADE_NAME);
+        }
     }
 }
