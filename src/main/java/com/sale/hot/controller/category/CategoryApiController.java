d32206749d852b48cb5dc94425a561d897383b06
CHANHAN <130114269+chanani@users.noreply.github.com>
2025-05-06 13:36:48 +0900

    feat : 카테고리 목록 조회 API 작업

--- src/main/java/com/sale/hot/controller/category/CategoryApiController.java
+++ src/main/java/com/sale/hot/controller/category/CategoryApiController.java
@@ -1,12 +1,32 @@
 package com.sale.hot.controller.category;
 
+import com.sale.hot.domain.category.service.CategoryService;
+import com.sale.hot.domain.category.service.dto.response.CategoriesResponse;
+import com.sale.hot.entity.category.Category;
+import com.sale.hot.global.annotation.NoneAuth;
+import com.sale.hot.global.response.DataResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 
 @Tag(name = "Category API Controller", description = "게시글 카테고리 관련 API를 제공합니다.")
 @RestController
 @RequiredArgsConstructor
 public class CategoryApiController {
 
+    private final CategoryService categoryService;
+
+    @Operation(summary = "카테고리 목록 API",description = "카테고리 목록을 조회합니다.")
+    @NoneAuth
+    @GetMapping("/api/v1/none/categories")
+    public ResponseEntity<DataResponse> notices() {
+        List<CategoriesResponse> categories = categoryService.getCategories();
+        return ResponseEntity.ok(DataResponse.send(categories));
+    }
 }
--- src/main/java/com/sale/hot/domain/category/service/DefaultCategoryService.java
+++ src/main/java/com/sale/hot/domain/category/service/DefaultCategoryService.java
@@ -1,13 +1,29 @@
 package com.sale.hot.domain.category.service;
 
+import com.sale.hot.domain.category.repository.CategoryRepository;
+import com.sale.hot.domain.category.service.dto.response.CategoriesResponse;
+import com.sale.hot.entity.category.Category;
+import com.sale.hot.entity.common.constant.BooleanYn;
+import com.sale.hot.entity.common.constant.StatusType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 @Slf4j
 public class DefaultCategoryService implements CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    @Override
+    public List<CategoriesResponse> getCategories() {
+        return categoryRepository.findAllByStatusAndActiveOrderByOrder(StatusType.ACTIVE, BooleanYn.Y).stream()
+                .map(CategoriesResponse::new)
+                .toList();
+    }
 }
