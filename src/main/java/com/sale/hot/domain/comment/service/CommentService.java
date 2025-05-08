6949e27edeccb97b67ff0f6e02440e0754c7855d
CHANHAN <130114269+chanani@users.noreply.github.com>
2025-05-08 22:05:03 +0900

    feat : 댓글 등록 API 작업

--- src/main/java/com/sale/hot/domain/comment/service/CommentService.java
+++ src/main/java/com/sale/hot/domain/comment/service/CommentService.java
@@ -1,8 +1,11 @@
 package com.sale.hot.domain.comment.service;
 
+import com.sale.hot.domain.comment.service.dto.request.CommentCreateRequest;
 import com.sale.hot.domain.comment.service.dto.response.CommentResponse;
+import com.sale.hot.entity.user.User;
 import com.sale.hot.global.page.Page;
 import com.sale.hot.global.page.PageInput;
+import jakarta.validation.Valid;
 
 import java.util.List;
 
@@ -14,4 +17,12 @@ public interface CommentService {
      */
     Page<List<CommentResponse>> getComments(PageInput pageInput, Long postId);
 
+    /**
+     * 댓글 등록
+     * @param postId 게시글 식별자
+     * @param user 로그인 사용자 객체
+     * @param request 댓글 등록 요청 객체
+     * @return 등록된 댓글 식별자
+     */
+    Long addComment(Long postId, User user, @Valid CommentCreateRequest request);
 }
