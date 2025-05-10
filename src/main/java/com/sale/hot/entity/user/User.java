05e940158eb07def41db356e85c09258dab59f46
CHANHAN <130114269+chanani@users.noreply.github.com>
2025-05-10 13:49:18 +0900

    feat : 해당 일 최초 로그인 시 회원 정보에 출석일 증가 chore : user Entity에 게시글 수, 댓글 수, 출석일 수 추가

--- src/main/java/com/sale/hot/entity/user/User.java
+++ src/main/java/com/sale/hot/entity/user/User.java
@@ -78,6 +78,15 @@ public class User extends BaseEntity {
     @Column(name = "social_id")
     private String socialId;
 
+    @Column(name = "post_count")
+    private Integer postCount;
+
+    @Column(name = "comment_count")
+    private Integer commentCount;
+
+    @Column(name = "attend_count")
+    private Integer attendCount;
+
     @Builder.Default
     @OneToMany(mappedBy = "user", cascade = CascadeType.PERSIST)
     private List<Notification> notifications = new ArrayList<>();
@@ -110,33 +119,34 @@ public class User extends BaseEntity {
     /**
      * 등급 변경
      */
-    public void addGrade(Grade grade){
+    public void addGrade(Grade grade) {
         this.grade = grade;
     }
 
     /**
      * 프로필 변경
      */
-    public void updateProfile(String profile){
+    public void updateProfile(String profile) {
         this.profile = profile;
     }
 
     /**
      * 비밀번호 변경
      */
-    public void updatePassword(String password){
+    public void updatePassword(String password) {
         this.password = new BCryptPasswordEncoder().encode(password);
     }
 
     /**
      * 연락처 변경
      */
-    public void addPhone(String phone){
+    public void addPhone(String phone) {
         this.phone = phone;
     }
 
     /**
      * 알림 등록
+     *
      * @param notification : 알림 엔티티
      */
     public void addNotification(Notification notification) {
@@ -147,7 +157,7 @@ public class User extends BaseEntity {
     /**
      * 게시글 등록
      */
-    public void addPost(Post post){
+    public void addPost(Post post) {
         this.posts.add(post);
         post.setUser(this);
     }
@@ -155,15 +165,15 @@ public class User extends BaseEntity {
     /**
      * 관심 게시글 등록
      */
-    public void addPostLike(PostLike postLike){
+    public void addPostLike(PostLike postLike) {
         this.postLikes.add(postLike);
         postLike.setUser(this);
     }
 
     /**
-     *  댓글 등록
+     * 댓글 등록
      */
-    public void addComment(Comment comment){
+    public void addComment(Comment comment) {
         this.comments.add(comment);
         comment.setUser(this);
     }
@@ -171,7 +181,7 @@ public class User extends BaseEntity {
     /**
      * 신고 등록
      */
-    public void addReport(Report report){
+    public void addReport(Report report) {
         this.reports.add(report);
         report.setUser(this);
     }
@@ -179,7 +189,7 @@ public class User extends BaseEntity {
     /**
      * 댓글 좋아요 / 싫어요 등록
      */
-    public void addCommentLike(CommentLike commentLike){
+    public void addCommentLike(CommentLike commentLike) {
         this.CommentLike.add(commentLike);
         commentLike.setUser(this);
     }
@@ -187,7 +197,7 @@ public class User extends BaseEntity {
     /**
      * 출석 등록
      */
-    public void addAttend(Attend attend){
+    public void addAttend(Attend attend) {
         this.attends.add(attend);
         attend.setUser(this);
     }
@@ -195,32 +205,62 @@ public class User extends BaseEntity {
     /**
      * 최근 접속일 수정
      */
-    public void updateLastVisit(){
+    public void updateLastVisit() {
         this.lastVisit = LocalDateTime.now();
     }
 
     /**
      * 회원 정보 수정
      */
-    public void update(User user){
-        if(user.name != null){
+    public void update(User user) {
+        if (user.name != null) {
             this.name = user.name;
         }
-        if(user.nickname != null){
+        if (user.nickname != null) {
             this.nickname = user.nickname;
         }
-        if(user.phone != null){
+        if (user.phone != null) {
             this.phone = user.phone;
         }
-        if(user.email != null){
+        if (user.email != null) {
             this.email = user.email;
         }
-        if(user.gender != null){
+        if (user.gender != null) {
             this.gender = user.gender;
         }
-        if(user.birth != null){
+        if (user.birth != null) {
             this.birth = user.birth;
         }
     }
+
+    /**
+     * 게시글 수 증가/감소 (true = 증가, false = 감소)
+     */
+    public void updatePostCount(boolean isType) {
+        if (isType) {
+            this.postCount += 1;
+        } else {
+            this.postCount -= 1;
+        }
+    }
+
+    /**
+     * 댓글 수 증가/감소 (true = 증가, false = 감소)
+     */
+    public void updateCommentCount(boolean isType) {
+        if (isType) {
+            this.commentCount += 1;
+        } else {
+            this.commentCount -= 1;
+        }
+    }
+
+    /**
+     * 출석일 수 증가
+     */
+    public void plusAttendCount() {
+        this.attendCount += 1;
+    }
+
 
 }
