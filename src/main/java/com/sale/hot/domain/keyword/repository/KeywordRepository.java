c9b252ad374201002b96896613ae01cd6052e9ca
CHANHAN <130114269+chanani@users.noreply.github.com>
2025-05-11 15:27:12 +0900

    feat : 알림 받을 키워드 등록 API 작업

--- src/main/java/com/sale/hot/domain/keyword/repository/KeywordRepository.java
+++ src/main/java/com/sale/hot/domain/keyword/repository/KeywordRepository.java
@@ -4,4 +4,5 @@ import com.sale.hot.entity.keyword.Keyword;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface KeywordRepository extends JpaRepository<Keyword, Long> {
+    boolean existsByUserIdAndName(Long id, String keyword);
 }
--- src/main/java/com/sale/hot/domain/keyword/service/DefaultKeywordService.java
+++ src/main/java/com/sale/hot/domain/keyword/service/DefaultKeywordService.java
@@ -1,5 +1,11 @@
 package com.sale.hot.domain.keyword.service;
 
+import com.sale.hot.domain.keyword.repository.KeywordRepository;
+import com.sale.hot.domain.keyword.service.dto.request.KeywordCreateRequest;
+import com.sale.hot.entity.keyword.Keyword;
+import com.sale.hot.entity.user.User;
+import com.sale.hot.global.exception.OperationErrorException;
+import com.sale.hot.global.exception.dto.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -10,4 +16,18 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 @Slf4j
 public class DefaultKeywordService implements KeywordService{
+
+    private final KeywordRepository keywordRepository;
+
+    @Override
+    @Transactional
+    public void addKeyword(KeywordCreateRequest request, User user) {
+        // 이미 등록되어 있는 키워드인지 확인
+        if(keywordRepository.existsByUserIdAndName(user.getId(), request.keyword())){
+            throw new OperationErrorException(ErrorCode.EXISTS_KEYWORD_NAME);
+        }
+        // 등록
+        Keyword newEntity = request.toEntity(user);
+        keywordRepository.save(newEntity);
+    }
 }
