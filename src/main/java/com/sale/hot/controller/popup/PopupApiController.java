fae875a241eec6569280a4efc45daf3aa676b186
CHANHAN <130114269+chanani@users.noreply.github.com>
2025-06-16 20:26:05 +0900

    chore : DB Connection 정보 변경

--- src/main/java/com/sale/hot/controller/popup/PopupApiController.java
+++ src/main/java/com/sale/hot/controller/popup/PopupApiController.java
@@ -2,20 +2,13 @@ package com.sale.hot.controller.popup;
 
 import com.sale.hot.domain.popup.service.PopupService;
 import com.sale.hot.domain.popup.service.dto.response.PopupsResponse;
-import com.sale.hot.domain.user.service.dto.request.UserUpdatePasswordRequest;
-import com.sale.hot.entity.user.User;
 import com.sale.hot.global.annotation.NoneAuth;
-import com.sale.hot.global.response.ApiResponse;
 import com.sale.hot.global.response.DataResponse;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
